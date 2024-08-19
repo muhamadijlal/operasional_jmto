d590191a9dd5457f8fffd64d1c26fcd611469e56
muhamadijlal <haidarijlal027@gmail.com>
2024-08-19 10:32:07 +0700

    [WEB OPR JMTO] - memperbaharui template import petugas, membuat function getJabatanId

--- app/Imports/PetugasImport.php
+++ app/Imports/PetugasImport.php
@@ -28,17 +28,40 @@ class PetugasImport implements ToCollection
         // START COLUMN ROW IN INDEX 1
         // $collection[2][1]
         for ($row = 2; $row < count($collection); $row++) {
+
+            $jabatan_id = $this->getJabatanId($collection[$row][3]);
+
             tbl_pegawai::create([
                 'npp_no' => $collection[$row][1],                   // NPP PETUGAS
                 'email' => $collection[$row][2],                    // EMAIL PETUGAS
-                'nama_pegawai' => $collection[$row][3],             // NAMA PETUGAS
-                'password' => Hash::make($collection[$row][4]),     // PASSWORD PETUGAS = NPP_NO
-                'jabatan_id' => 3                                   // JABATAN PETUGAS
+                'jabatan_id' => $jabatan_id,                        // JABATAN PETUGAS
+                'nama_pegawai' => $collection[$row][4],             // NAMA PETUGAS
+                'password' => Hash::make($collection[$row][5]),     // PASSWORD PETUGAS = NPP_NO
             ]);
         }
     }
 
     public function getFailed(){
         return $this->errors;
+    }
+
+    private function getJabatanId($jabatan){
+        $jabatan_id = '';
+        $jabatan = strtolower($jabatan);
+
+        switch($jabatan) {
+            case 'kbt':
+                $jabatan_id = 1;
+            case 'kspt':
+                $jabatan_id = 2;
+            case 'plt':
+                $jabatan_id = 3;
+            case 'teknisi':
+                $jabatan_id = 4;
+            default:
+                break;
+        }
+
+        return $jabatan_id;
     }
 }
