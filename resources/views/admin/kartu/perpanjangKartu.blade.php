2104835a99bf96507ada9079f87cd8ac1eee39ce
muhamadijlal <haidarijlal027@gmail.com>
2024-08-26 21:00:09 +0700

    [WEB OPR JMTO] - Membuat fungsi read RFID pada menu perpanjangan

--- resources/views/admin/kartu/perpanjangKartu.blade.php
+++ resources/views/admin/kartu/perpanjangKartu.blade.php
@@ -105,242 +105,370 @@ Perpanjang Kartu
     </form>
   </div>
 </div>
+
+<!-- Modal-->
+<div class="modal fade" id="kartuOperasionalModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+      <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h3 id="petugas-modal-tittle" class="modal-title">Tambah jadwal</h3>
+          </div>
+          <div class="modal-body">
+              <form id="form-kartu-operasional">
+                  <div class="form-group">
+                      <label for="exampleFormControlTextarea1">Response :</label>
+                      <textarea class="form-control" id="response" rows="8" readonly></textarea>
+                  </div>
+              </form>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-dismiss="modal">Tutup</button>
+
+          </div>
+      </div>
+
+  </div>
+</div>
 @endsection
 
 @section('js')
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script src="{{ asset('assets/js/admin/kartu.js') }}"></script>
-{{-- <script>
-  baseUrl = '{{ url()->current() }}'
+<script>
+baseUrl = '{{ url()->current() }}'
 
 
-  var dataObject = eval('<?php echo json_encode($Columns); ?>')
-  var table = ''
-  var ModalTambahKartu = $("#ModalTambahKartu");
+$(document).ready(function() {
+  var api = IOTClientService;
+  var status = false;
+  var write_status = false;
 
-  function sweetAlert(title, text, icon) {
-    Swal.fire({
-      title: title,
-      text: text,
-      icon: icon,
-      customClass: {
-        confirmButton: 'btn btn-primary'
-      },
-      buttonsStyling: false
-    });
-  }
+  // Set up callbacks
+  api.onconnect = function() {
+    console.log("WebSocket connected");
+    status = true;
+  };
 
-  $(document).ready(function () {
-    $('#submit-btn').on('click', function () {
-      table.ajax.reload();
-    });
-  });
+  api.ondisconnect = function() {
+    console.log("WebSocket disconnected");
+    status = false;
+  };
 
-  table = $('#tbl_list').DataTable({
-    processing: true,
-    serverSide: true,
-    ajax: {
-      url: baseUrl, // Ganti dengan URL yang sesuai
-      type: 'GET',
-      data: function(d){
-        d.kategori_id = $("#kategori").val();
-      }
-    },
-    columns: dataObject,
-    displayLength: 10,
-    scrollX: true,
-    scrollCollapse: true,
-    order: [
-      [4, 'desc']
-    ],
-    orderCellsTop: true,
-    lengthMenu: [
-      [10, 25, 50, -1],
-      ['10 rows', '25 rows', '50 rows', 'Show all']
-    ],
-    language: {
-      emptyTable: "Tidak ada data yang tersedia"
-      // Atur pesan lain sesuai kebutuhan Anda
+  api.onlog = function(log) {
+    console.log("Log:", log);
+
+    if(log == '[*] disconnected') {
+      $("#com i").removeClass("text-success").addClass("text-danger");
+      $("#cst i").removeClass("text-success").addClass("text-danger");
+      $("#cst_type" ).html('');
+      $("#service i").removeClass("text-success").addClass("text-danger");
+      $("#uid").html('?');
+      write_aktif(false);
+    } else {
+      $("#service i").removeClass("text-danger").addClass("text-success");
     }
-  });
+  };
 
-  $("#ruas-ktp").change(function(){
-    var a = $("#ruas-ktp").val();
-    var no_kartu = a[0];
-    var fisik_kartu = a[1];
-    
-    var b = $("#nomor_kartu").val();
-    if(a == '0'){return false;}
+  var cst = false;
+  var have_card = false;
+  var com = '';
+  var tipe_cst = '';
+  var curr_uuid = '';
 
-    showGenerateNoKartu();
- });
+  api.onmessage = function(msg) {
+    if ('active' in msg) {
+      if (msg.active != cst) {
+        cst = msg.active;
+        if (cst){
+          $("#cst i").removeClass("text-danger").addClass("text-success");
+          $("#cst_type").html(': '+ msg.type);
+        } else {
+          $("#cst i").removeClass("text-success").addClass("text-danger");
+          $("#cst_type" ).html('');
+          $("#uid" ).html('?');
+          write_aktif(false);
+        }
+      }
+      if (!msg.active) {
+        msg.havecard=false;
+      }
+    }
 
- $("#institusi").change(function(){ 
-    var a = $("#ruas-ktp").val();
-    var no_kartu = a[0];
-    var fisik_kartu = a[1];
-    
-    var b = $("#nomor_kartu").val();
-    if(a == '0'){return false;}
+    if ('com' in msg) {
+      if(msg.com != com) {
+        $("#com i").removeClass("text-danger").addClass("text-success");
+        $("#com i + span").text(msg.com)
+      } else {
+        $("#com i").removeClass("text-success").addClass("text-danger");
+        $("#uid" ).html('?');
+        write_aktif(false);
+      }
+    }
 
-    showGenerateNoKartu();
- });
+    if ('havecard' in msg) {
+      if(msg.uid != curr_uuid) {
+        curr_uuid = msg.uid;
+        if(curr_uuid) {
+          $("#uid").html('UID : ' + curr_uuid);
+          document.getElementById('btnTulis').uid = curr_uuid;
+          $('#btnTulis').prop('disabled', false);
 
- $("#unit").change(function(){
-    var a = $("#ruas-ktp").val();
-    var no_kartu = a[0];
-    var fisik_kartu = a[1];
-    
-    var b = $("#nomor_kartu").val();
-    if(a == '0'){return false;}
-
-    showGenerateNoKartu();
- });
-
- function showGenerateNoKartu()
-  {
-    var ruas = $('#ruas-ktp').val() || '';
-    var institusi = $('#institusi').val() || '';
-    var unit = $('#unit').val() || '';
-
-
-    var join = institusi + ruas + unit;
-    //console.log(join);
-    $('#nomor_kartu').val(join);
-
+          showKartuDinas();
+          write_aktif(true);
+        } else {
+          write_aktif(false);
+          $("#uid").html(' -');
+          document.getElementById('btnTulis').uid = '';
+          $('#btnRead').prop('disabled',true);
+          $('#btnTulis').prop('disabled', true);
+        }
+      }
+    }
   }
 
-  $("#btnRefresh").on('click', function() {
-    table.draw();
-  })
+  api.open();
 
-  $("#btnTambahKartu").on('click', function() {
-    ModalTambahKartu.modal('show')
-  })
+  $("#btnRead").click(function() {
+    window.history.forward(1);
 
-  $("#btnSumbitTambahPetugas").on('click', function(){
-    var nomor_kartu = $("#nomor_kartu").val()
-    var no_ref = $("#no_ref").val()
-    var pemilik_kartu = $("#pemilik_kartu").val()
-    var ruas = $("#ruas-ktp").val()
-    var jenis_ktp = $("#jenis_ktp").val()
-    var institusi = $("#institusi").val()
-    var unit = $("#unit").val()
-    var tgl_kadaluarsa = $("#tgl_kadaluarsa").val()
+    var data = document.getElementById('btnRead').dataktp;
 
-    var formData = new FormData();
-
-    formData.append('nomor_kartu', nomor_kartu);
-    formData.append('no_ref', no_ref);
-    formData.append('pemilik_kartu', pemilik_kartu);
-    formData.append('ruas', ruas);
-    formData.append('jenis_ktp', jenis_ktp);
-    formData.append('institusi', institusi);
-    formData.append('unit', unit);
-    formData.append('tgl_kadaluarsa', tgl_kadaluarsa);
-    formData.append('_token', '{{ csrf_token() }}');
+    var blok0 = data[0];
+    var blok1 = data[1];
+    var blok2 = data[2];
 
     $.ajax({
-      type: "POST",
-      contentType: false,
-      processData: false,
-      data: formData,
-      url: baseUrl + '/tambah',
-      async: false,
-      beforeSend: function () {
-        // document.getElementById('loading-screen').style.display = 'block';
+      url: base_url + '/getDetailKTP',
+      method: "POST",
+      dataType: 'json',
+      data: {
+        blok0: blok0,
+        blok1: blok1,
+        blok2: blok2
       },
-      success: function (response) {
-        if (response.status == 200) {
-          ModalTambahKartu.modal('hide')
-          table.draw();
+      beforeSend: function() {
+        document.getElementById('loading-screen').style.display = 'block';
+      },
+      success: function(response) {
+        console.log(response);
+        var ruas = (response.data.ruas).toUpperCase();
+        var expired = (response.data.expire).substring(0, 4) + '-' + (response.data.expire).substring(4, 6) + '-' + (response.data.expire).substring(6, 8);
+        var tipe = response.data.tipe;
+        var nokartu = response.data.nokartu;
+        var uid = (response.data.uid).toUpperCase();
+        var namaa = (response.ktpNama[0].nama).toUpperCase();
+        var nama;       
+
+        swal.fire({
+          title: 'Data Kartu Tol Perusahaan',
+          customClass: {
+            popup: 'swal-wide',
+            closeButton: 'clse',
+          },
+          showCloseButton: true,
+          background: '#add5e3',
+          showConfirmButton: false,
+          footer: '<a href="https://www.jmto.co.id/">Butuh bantuan untuk memahami aplikasi ?</a>',
+          html: `<table width="100%"  border="0" class="tble">
+            <style>
+            .uid_img{
+              width: 250px;
+            }
+            .odd{
+              background-color:#7bb3c1;
+            }
+            .clse{
+              color:black;
+            }
+            .even{
+              background-color:#7bb3c1;
+            }
+            .tble{
+              background-color:#add5e3;
+              color:black; font-weight:bold;  
+            }
+            </style>
+            <thead>
+            <tr style="padding:10px;">
+              <td width="25%" rowspan="6"><img class="uid_img" src="` + base_url + `assets/file/logo/card.png"></img></td>
+              <td class="" width="20%" style="text-align:left;">UID</td>
+              <td width="2%" style="text-align:left;">:</td>
+              <td width="" style="text-align:left;">` + uid + `</td>
+            </tr>
+        
+            <tr>
+              <td style="text-align:left;">Registrasi</td>
+              <td style="text-align:left;">:</th>
+              <td style="text-align:left;">` + nokartu + `</td>
+            </tr>
+        
+            <tr>
+              <td style="text-align:left;">Nama</td>
+              <td style="text-align:left;">:</th>
+              <td style="text-align:left;">` + namaa + `</td>
+            </tr>
+            <tr>
+              <td style="text-align:left;">Ruas</td>
+              <td style="text-align:left;">:</th>
+              <td style="text-align:left;">` + tipeRuas(ruas) + `</td>
+            </tr>
+            <tr>
+              <td style="text-align:left;">Jenis</td>
+              <td style="text-align:left;">:</th>
+              <td style="text-align:left;">Kartu ` + tipeKartu(tipe) + `</td>
+            </tr>
+            <tr>
+              <td style="text-align:left;">Expired</td>
+              <td style="text-align:left;">:</th>
+              <td style="text-align:left;">` + expired + `</td>
+            </tr>
+            </thead>
+          </table>`
+        });
+
           document.getElementById('loading-screen').style.display = 'none';
-          sweetAlert('Berhasil!', response.message, 'success')
+      },
+      error: function(jqXHR, exception) {
+        document.getElementById('loading-screen').style.display = 'none';
+        if (jqXHR.status === 0) {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Not connect.\n Verify Network.'
+          })
+        } else if (jqXHR.status == 404) {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Requested page not found. [404]'
+          })
+        } else if (jqXHR.status == 500) {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Internal Server Error [500].'
+          })
+        } else if (exception === 'parsererror') {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Requested JSON parse failed.'
+          })
+        } else if (exception === 'timeout') {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Time out error.'
+          })
+        } else if (exception === 'abort') {
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Ajax request aborted.'
+          })
         } else {
-          document.getElementById('loading-screen').style.display = 'none';
-          sweetAlert('Gagal!', response.message + " error : " + response.error, 'error')
+          Swal.fire({
+            type: 'error',
+            title: 'DATA TIDAK TER RELOAD DENGAN BAIK',
+            text: 'Uncaught Error.\n' + jqXHR.responseText
+          })
         }
-      },
-    })
-  })
-
-  $('.datatables-basic').on('click', '#blacklist', function(){
-    var ktp_id = $(this).data('id');
-
-    Swal.fire({
-      title: 'Blacklist KTP ?',
-      text: "KTP akan diblacklist diseluruh gerbang.",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Blacklist',
-      cancelButtonText: 'Batal',
-      customClass: {
-        confirmButton: 'btn btn-danger me-3',
-        cancelButton: 'btn btn-label-secondary'
-      },
-      buttonsStyling: false
-    }).then(function (result) {
-      if(result.isConfirmed){
-        $.ajax({
-          url: baseUrl+'/blacklist/'+ktp_id,
-          method: "POST",
-          headers: {
-            'X-CSRF-TOKEN': '{{ csrf_token() }}'
-          },
-          contentType: false,
-          cache: false,
-          processData: false,
-          success: function (response) {
-            // document.getElementById('loading-screen').style.display = 'block';
-            setTimeout(function () {
-                table.ajax.reload();
-                // document.getElementById('loading-screen').style.display = 'none';
-                sweetAlert('Berhasil!', response.message, 'success')
-            }, 1000);
-          }
-        });
       }
     });
   })
 
-  $('.datatables-basic').on('click', '#whitelist', function(){
-    var ktp_id = $(this).data('id');
+  $("#btnService").on('click', function(){         
+    var fileUrl = "{{ asset('assets/file/ClientService.exe') }}";
+    // Now you can use this URL in JavaScript
+    location.href = fileUrl;
+  });
 
-    Swal.fire({
-      title: 'Whitelist KTP ?',
-      text: "KTP akan diwhitelist diseluruh gerbang.",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Whitelist',
-      cancelButtonText: 'Batal',
-      customClass: {
-        confirmButton: 'btn btn-success me-3',
-        cancelButton: 'btn btn-label-secondary'
-      },
-      buttonsStyling: false
-    }).then(function (result) {
-      if(result.isConfirmed){
-        $.ajax({
-          url: baseUrl+'/whitelist/'+ktp_id,
-          method: "POST",
-          headers: {
-            'X-CSRF-TOKEN': '{{ csrf_token() }}'
-          },
-          contentType: false,
-          cache: false,
-          processData: false,
-          success: function (response) {
-            // document.getElementById('loading-screen').style.display = 'block';
-            setTimeout(function () {
-                table.ajax.reload();
-                // document.getElementById('loading-screen').style.display = 'none';
-                sweetAlert('Berhasil!', response.message, 'success')
-            }, 1000);
-          }
-        });
-      }
-    });
-  })
-</script> --}}
+  function tipeKartu(id) {
+    var kartu = '';
+
+    switch (id) {
+      case '01':
+        kartu = 'Operasional';
+        break;
+      case '02':
+        kartu = 'Karyawan';
+        break;
+      case '03':
+        kartu = 'Mitra';
+        break;
+      default:
+        kartu = 'UNKNOWN';
+        break;
+    }
+
+    return kartu;
+  }
+
+  function tipeRuas(id) {
+    var kartu = '';
+    switch (id.toUpperCase()) {
+      case 'A045':
+        kartu = 'MTN';
+        break;
+      case 'A047':
+        kartu = 'MTN + JANGER';
+        break;
+      case 'A04D':
+        kartu = 'MTN + BSD';
+        break;
+      case 'A04F':
+        kartu = 'MTN + JANGER + BSD';
+        break;
+      case 'A050':
+        kartu = 'JKC';
+        break;
+      case 'A052':
+        kartu = 'JKC + JANGER';
+        break;
+      case 'A024':
+        kartu = 'CSJ';
+        break;
+      case 'A02C':
+        kartu = 'CSJ+BSD';
+        break;
+      case 'A07F':
+        kartu = 'JORR 2 + BSD + JANGER';
+        break;
+      case 'A075':
+        kartu = 'MTN + CSJ + JKC';
+        break;
+      case 'A047':
+        kartu = 'MTN + JANGER';
+        break;
+      case 'A077':
+        kartu = 'JORR 2 + JANGER';
+        break;
+      default:
+        kartu = 'Unknown1';
+        break;
+    }
+
+    return kartu;
+  }
+
+  async function showKartuDinas() {
+    var data = await api.read_sector(7, 'A', '2177A6F53421');
+    if (data) {
+      //console.log(data);
+      api.beep();
+      api.beep();
+      $('#btnRead').prop('disabled', false);
+      //window.data_paspul=data;
+      document.getElementById('btnRead').dataktp = data;
+
+    } else {
+      //console.log('Bukan Kartu Dinas');
+      api.beep();
+      $('#btnRead').prop('disabled', true);
+    }
+  }
+})
+</script>
 @endsection
