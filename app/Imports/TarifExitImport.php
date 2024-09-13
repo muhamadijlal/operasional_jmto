fa2a36a2460b92a9edd753c73f1abe49d2c2fa29
93 <aziz.fiqri@gmail.com>
2024-09-13 18:38:10 +0700

    fix import tarif

--- app/Imports/TarifExitImport.php
+++ app/Imports/TarifExitImport.php
@@ -36,7 +36,7 @@ class TarifExitImport implements ToCollection
                 }
             }
             if ($i >= 2) {
-                $asal_gerbang = $rows[$i][0];
+                $asal_gerbang = strtolower($rows[$i][0]);
                 $jenis = $rows[$i][1];
 
                 $kelipatanGetGol = 1;
@@ -90,7 +90,7 @@ class TarifExitImport implements ToCollection
                 }
 
                 try {
-                    $modelAsalGerbang = DB::connection('mysql')->table('tbl_gerbang')->where('gerbang_nama')->first();
+                    $modelAsalGerbang = DB::connection('mysql')->table('tbl_gerbang')->where('gerbang_nama', $asal_gerbang)->first();
 
                     if (!$modelAsalGerbang) {
                         throw new ModelNotFoundException("Data dengan nama gerbang '{$asal_gerbang}' tidak ditemukan.");
@@ -114,7 +114,7 @@ class TarifExitImport implements ToCollection
                         'ruas_id' => $this->gerbang->ruas_id,
                         'gerbang_id' => $this->gerbang->gerbang_id,
                         'asal_gerbang' => $modelAsalGerbang->gerbang_id,
-                        'jenis' => $jenis,
+                        'jenis' => $modelAsalGerbang->jenis,
                         'gol1' => $gol1,
                         'gol1_d' => '[' . implode(',', $gol1_d) . ']',
                         'gol2' => $gol2,
@@ -129,6 +129,7 @@ class TarifExitImport implements ToCollection
                         'id_dasar_tarif' => $this->request->dasartarifmodal,
                         'aktif' => 1,
                         'tarif_inv' => '[' . implode(',', str_replace('"', '', $investors)) . ']',
+                        save();
                     ]);
 
                 } else {
